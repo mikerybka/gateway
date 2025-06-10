da0a6c01570ef3f307c91854313c42f4a9a7a920
Mike Rybka <merybka@gmail.com>
2025-06-10 19:18:23 -0400

    Create gateway

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/mikerybka/gateway
+
+go 1.23.5
+
+require golang.org/x/crypto v0.39.0
+
+require (
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"path/filepath"
+
+	"golang.org/x/crypto/acme/autocert"
+)
+
+func main() {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		home = "/root"
+	}
+	configPath := filepath.Join(home, "gateway.json")
+	certPath := filepath.Join(home, "certs")
+	b, err := os.ReadFile(configPath)
+	if err != nil {
+		panic(err)
+	}
+	conf := map[string]string{}
+	err = json.Unmarshal(b, &conf)
+	if err != nil {
+		panic(err)
+	}
+	hosts := []string{}
+	mux := http.NewServeMux()
+	for host, backendURL := range conf {
+		hosts = append(hosts, host)
+		u, err := url.Parse(backendURL)
+		if err != nil {
+			panic(err)
+		}
+		mux.Handle(fmt.Sprintf("%s/", host), httputil.NewSingleHostReverseProxy(u))
+	}
+	m := &autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		Cache:      autocert.DirCache(certPath),
+		HostPolicy: autocert.HostWhitelist(hosts...),
+	}
+	server := &http.Server{
+		Addr: ":443",
+		TLSConfig: &tls.Config{
+			GetCertificate: m.GetCertificate,
+		},
+		Handler: mux,
+	}
+	go func() {
+		http.ListenAndServe(":80", m.HTTPHandler(nil))
+	}()
+	log.Fatal(server.ListenAndServeTLS("", "")) // Cert and key are managed by autocert
+}
